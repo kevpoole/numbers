6b9d84ecc4561e7a923b1ac8fbeebf4744a485d8
Kevin Poole <kevitron@hotmail.com>
2024-08-11 20:35:04 -0400

    limit 100

--- src/components/Num.tsx
+++ src/components/Num.tsx
@@ -9,7 +9,7 @@ function Num({num}: NumProps) {
     const [isVisible, setIsVisible] = useState(true)
   return (
     <>
-    {isVisible && <div onClick={() => setIsVisible(false)} className="h-8 py-8 px-4 text-2xl flex items-center justify-center border border-green-500 ">{num}</div>}
+    {isVisible && <div onClick={() => setIsVisible(false)} className="h-8 py-8 px-4 text-2xl no-underline flex items-center justify-center border border-green-500 ">{num}</div>}
     {!isVisible && <div   onClick={() => setIsVisible(true)}className="h-8 py-8 px-4 text-2xl flex items-center justify-center "></div>}
     </>
   )
